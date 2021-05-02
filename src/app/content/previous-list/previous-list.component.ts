db6deac9f0a39de86e26add0d630b6903a6a44a0
Deepak Jain <deepak.pce1992@gmail.com>
2021-05-02 13:44:19 +0530

    Allow copying items from previous list

--- src/app/content/previous-list/previous-list.component.ts
+++ src/app/content/previous-list/previous-list.component.ts
@@ -8,11 +8,33 @@ import { previousItem } from "../item.model";
   styleUrls: ["./previous-list.component.css"],
 })
 export class PreviousListComponent implements OnInit {
+  // previousItems: previousItem[] = [
+  //   {
+  //     id: 1,
+  //     date: new Date(),
+  //     items: [
+  //       {
+  //         id: 1,
+  //         itemName: "Rice",
+  //         itemQuantity: 2,
+  //         itemUnit: "Kg",
+  //         itemAmount: 200,
+  //       },
+  //     ],
+  //     totalAmount: 400,
+  //   },
+  // ];
   previousItems: previousItem[] = [];
   //previousItemSub: Subscription;
   constructor(public previousItemService: PreviousItemService) {}
 
   ngOnInit() {
     this.previousItems = this.previousItemService.get();
+  }
+
+  onCopy(previousListId: number) {
+    if (confirm(`Do you want to copy this list's item into current list ?`)) {
+      this.previousItemService.copyItemToCurrentList(previousListId);
+    }
   }
 }
